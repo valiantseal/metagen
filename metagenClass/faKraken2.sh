8971ccbc612f2197d0a9a9faee31a4c6ebad17b7
abombin <abombin93@gmail.com>
2022-09-13 20:46:26 -0400

    removed kraken2 log

--- metagenClass/faKraken2.sh
+++ metagenClass/faKraken2.sh
@@ -9,7 +9,6 @@ conda activate kraken2
 mkdir kraken2
 for i in *.fa;
 do
-echo "$i" >> ../../kraken2.log
 kraken2 --use-names --threads 8 --db /home/ubuntu/kraken2/viral \
 --report ./kraken2/"$i".report  "$i" > ./kraken2/"$i".kraken
 done
