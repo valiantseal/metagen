8f7db7f361004791a54c898613e558da59c07570
abombin <abombin93@gmail.com>
2022-09-19 00:07:51 +0000

    added function to record s3 storage for a project

--- metagenClass/outputS3.sh
+++ metagenClass/outputS3.sh
@@ -2,3 +2,7 @@ run=${PWD##*/}
 
 aws s3 cp --recursive output s3://transfer-files-emory/metagenClass/"$run"/
 
+
+echo s3://transfer-files-emory/metagenClass/"$run"/ > download.me
+
+aws s3 cp download.me s3://transfer-files-emory/
