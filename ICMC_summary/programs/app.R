e42add51c96bd7ec1e5bf7e0ebc2c16755589c3e
abombin <abombin93@gmail.com>
2022-10-26 19:12:56 +0000

    added option to select only one column for summary

--- ICMC_summary/programs/app.R
+++ ICMC_summary/programs/app.R
@@ -9,8 +9,10 @@
 library(shiny)
 library(DT)
 
-allSamples<-read.csv('/home/ubuntu/ICMC/summaries/combined/allSamples.csv')
+allSamples<-read.csv('../combined/allSamples.csv')
+allSamples$Original_species<-gsub("_", " ", allSamples$Original_species)
 
+var2Choices<-c('None', colnames(allSamples)[2:ncol(allSamples)])
 
 linebreaks <- function(n){HTML(strrep(br(), n))}  # introduce multiple breaks in one function
 
@@ -22,7 +24,7 @@ ui <- navbarPage("Summary",
                             fluidPage(
                               mainPanel( 
                                 selectInput(inputId = "summaryVar1", label= "Column 1", choices = c(colnames(allSamples)[2:ncol(allSamples)]), selected='Name'), 
-                                selectInput(inputId = "summaryVar2", label= "Column 2", choices = c(colnames(allSamples)[2:ncol(allSamples)]), selected='Project'),
+                                selectInput(inputId = "summaryVar2", label= "Column 2", choices = var2Choices, selected='None'),
                                 div(DT::dataTableOutput("NameProjectSummary", width = "100%", height = "auto"), style = "font-size:115%"),
                                 linebreaks(3),
                               ))),
@@ -41,21 +43,29 @@ server <- function(input, output) {
   
   
   tableSummary<-reactive({
-    df<-data.frame(table(allSamples[,input$summaryVar1], allSamples[,input$summaryVar2]))
-    sumFiltr<-df[!(df$Freq==0),]
+    if (input$summaryVar2 == 'None'){
+      
+      df<-data.frame(table(allSamples[,input$summaryVar1]))
+      sumFiltr<-df[!(df$Freq==0),]
+      
+    } else {
+      
+      df<-data.frame(table(allSamples[,input$summaryVar1], allSamples[,input$summaryVar2]))
+      sumFiltr<-df[!(df$Freq==0),]
+      
+    }
+    
     return(sumFiltr)
   })
   
   output$NameProjectSummary <- DT::renderDataTable(tableSummary(), 
-    options = list(scrollX = TRUE), rownames= FALSE)
+                                                   options = list(scrollX = TRUE), rownames= FALSE)
   
   
   # table with all the samples
   output$AllSamples<- DT::renderDataTable(allSamples, 
-    options = list(scrollX = TRUE), rownames= FALSE)
+                                          options = list(scrollX = TRUE), rownames= FALSE)
   
 }
 
 shinyApp(ui, server)
-  
-  
--- ICMC_summary/programs/runShiny.R
+++ ICMC_summary/programs/runShiny.R
@@ -3,6 +3,6 @@ setwd("./")
 #print(" app is running on:  http://10.98.99.46:4000/")
 #runApp(appDir="./", host = getOption("shiny.host", "10.98.99.46"), port=4000)
 
-runApp(appDir="./", host = getOption("shiny.host", "10.66.123.250"), port=4000)
+runApp(appDir="./", host = getOption("shiny.host", "10.98.99.46"), port=4000)
 
 
