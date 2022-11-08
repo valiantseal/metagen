b53653985cee776e835af93fbaa22c6c6bf1a8ad
Ubuntu <ubuntu@ip-10-98-99-252.ec2.internal>
2022-11-08 13:51:15 +0000

    bactopia hybrid assembly

---
+++ ICMC/programs/development/bactopiaHybrid.sh
@@ -0,0 +1,6 @@
+bactopia --sample CDE_3 \
+--R1 ./input/CDE_3_R1.fastq.gz \
+--R2 ./input/CDE_3_R2.fastq.gz \
+--SE /home/ubuntu/ICMC/Ahmed/10.25-K.pneumoniae/all_data/028_000480_nanopore.fastq.gz \
+--hybrid \
+--max_cpus 32
