2ca7924599830345537b7a30c9d077a24301d922
abombin <abombin93@gmail.com>
2023-03-06 22:47:08 +0000

    find numb of chuncks for virema

--- ARVAR/viremaPipe/v1.1/programs/bin/splitFiltered.sh
+++ ARVAR/viremaPipe/v1.1/programs/bin/splitFiltered.sh
@@ -2,10 +2,13 @@ rm -rf splitSeq
 
 mkdir splitSeq
 
-seqkit split virema1.fastq -s 366220 -j 8 -O ./splitSeq/
+lines=$(cat virema1.fastq | wc -l)
+
+parts=$(echo "$lines"/4/95+5|bc)
+echo "$parts"
+
+seqkit split virema1.fastq -s "$parts" -j 4 -O ./splitSeq/
 
 cd splitSeq
-
-ls | wc -l 
 
 for j in *.fastq; do mkdir "$j"_dir; mv "$j" ./"$j"_dir/reads.fastq; done
