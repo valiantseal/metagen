6232e1b96d3083ca2822de28bf20759b31c647fa
abombin <abombin93@gmail.com>
2023-03-21 19:27:46 +0000

    download dir by current dir

--- viralreconAdapt/hrtv/programs/download.sh
+++ viralreconAdapt/hrtv/programs/download.sh
@@ -5,7 +5,11 @@ project="HRTV"
 
 dx select "$project"
 
-dir="2023-01-31"
+#dir="2023-03-13"
+
+fullPath=$(pwd)
+
+dir=$(echo "$fullPath" | cut -f7 -d "/")
 
 dx download "$dir"/1_raw_data/*.gz -o ./input/
 
