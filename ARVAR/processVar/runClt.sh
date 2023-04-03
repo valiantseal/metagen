318f16ef41a4b3a5b083bc2e66a56cb33fd4fff3
abombin <abombin93@gmail.com>
2023-04-03 21:47:25 +0000

    test flags

--- ARVAR/processVar/runClt.sh
+++ ARVAR/processVar/runClt.sh
@@ -4,3 +4,8 @@ python ./programs/getVarFilesCLT.py --help
 
 python ./programs/getVarFilesCLT.py -i input/output.sam \
 -r references/MN908947.3.fna
+
+time python ./programs/getVarFilesCLT.py -i test_rose/GA-EHC-2884X_L1_hst2.sam \
+-r test_rose/SARSCov2_Ref.fasta -t 8 -a 0.001
+
+python ./programs/test_GetVarFiles.py
