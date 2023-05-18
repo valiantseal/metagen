06f6e70b74a44d1b30b86e427e58754c8cfe54db
abombin <abombin93@gmail.com>
2023-05-18 17:18:17 -0400

    adjust pvalues based on presence in kegg pathways

---
+++ scRNA/linux/programs/paper_figs/Fig2/adjPvals.R
@@ -0,0 +1,71 @@
+source('../programs/renameClusters.R')
+
+library(ggplot2)
+library(viridis)
+library(Hmisc)
+library(plyr)
+
+targDir = './Paper_figs/Fig2/'
+dir.create(targDir, recursive = T, showWarnings = F)
+
+clusters<-c('SUB', 'CA1', 'CA2', 'CA3', 'DG', 'GABA', 'C-R', 'OPC', 'ODC', 'MG')
+
+curDate<-Sys.Date()
+
+levels(RNA.combined.norm)
+
+DefaultAssay(RNA.combined.norm)
+
+groupMarkers = read.csv("allDiffExprLogfc0.25_ContrVsStress_2022-10-25.csv")
+keggMark = read.csv('AUCell_KeggClustProF_All_ContrVsStress_2022-11-28.csv')
+keggdb = read.csv("keggMouse_clustprof_db.csv")
+
+# manually adjust p values
+adjPval = function(df, cluctCol, pCol) {
+  combDat = data.frame(matrix(nrow = 0, ncol = 0))
+  clusters = unique(df[, cluctCol])
+  for (cluster in clusters) {
+    dfSub = df[(df[cluctCol] == cluster),]
+    dfSub$fdr_p = p.adjust(dfSub[, pCol], method = 'fdr')
+    dfSub$bonferroni_p = p.adjust(dfSub[, pCol], method = 'bonferroni')
+    combDat = rbind(combDat, dfSub)
+  }
+  return(combDat)
+}
+
+markAdj = adjPval(df = groupMarkers, cluctCol = "Cell_Type", pCol = "p_val")
+#write.csv(markAdj, paste0(targDir, 'allDiffExprLogfc0.25_ContrVsStress_AdjP_', curDate, "csv"), row.names = F)
+
+allGenes = unique(row.names(RNA.combined.norm))
+allCurRes = unique(groupMarkers$Genes)
+
+getKeggSignGenes = function(df, db, cluctCol, pCol, genesDf) {
+  curFile <- file(paste0(targDir, 'checkKeggGenesCurDE_', curDate, ".txt"), "w")
+  combDat = data.frame(matrix(nrow = 0, ncol = 0))
+  clusters = unique(df[, cluctCol])
+  for (cluster in clusters) {
+    dfSub = df[(df[cluctCol] == cluster) & (df[pCol] < 0.05) ,]
+    keggList = unique(dfSub$Genes)
+    dbSub = db[(db$Kegg_path %in% keggList),]
+    genesList = unique(dbSub$Gene)
+    genesDfSub = genesDf[(genesDf$Genes %in% genesList),]
+    line1 = paste("Cluster", cluster, 'need ', length(genesList), sep = "  ")
+    line2 = paste("Cluster", cluster, 'found in current results', length(unique(genesDfSub$Genes)), sep = "  ")
+    line3 = paste("Cluster", cluster, 'found in current all Genes', length(unique(genesList[genesList%in% allGenes])), sep = "  ")
+    line4 = paste("Cluster", cluster, 'found in current all Rsults', length(unique(genesList[genesList%in% allCurRes])), sep = "  ")
+    # write in the file
+    print(line1)
+    print(line2)
+    print(line3)
+    print(line4)
+    lines <- c(line1, line2, line3, line4)
+    writeLines(lines, curFile)
+    curData = data.frame(genesList)
+    curData$Cluster = cluster
+    combDat = rbind(combDat, curData)
+  }
+  close(curFile)
+  return(combDat)
+}
+
+genesOfInt = getKeggSignGenes(df = keggMark, db = keggdb, cluctCol = "Cell_Type", pCol = "p_val", genesDf = groupMarkers)
