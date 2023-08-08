2ede54d4776163d272944032e8fcafce31f501f6
abombin <abombin93@gmail.com>
2023-08-08 16:59:43 +0000

    select metaseq files to download per library

--- ARVAR/iSNVs/matchDxSamples.R
+++ ARVAR/iSNVs/matchDxSamples.R
@@ -54,6 +54,43 @@ addDx = function(mainDf, dxDf) {
   return(mainDf)
 }
 
+# dxMissing = function(df) {
+#   df$Missing_meta_dx = NA
+#   for ( i in 1:nrow(df) ) {
+#     metaseqStr = df$Missing_Metaseq[i]
+#     if (!is.na(metaseqStr)) {
+#       curMetaseq = strsplit(metaseqStr, ";")[[1]]
+#       curDx = strsplit(df$Meta_dx[i], ";")[[1]]
+#       curMissingMetaseq = character()
+#       for (curSeq in curMetaseq) {
+#         curSeqCh = strsplit(curSeq, "_")[[1]]
+#         curMainStr = curSeqCh[1:3]
+#         curMainStr = paste(curMainStr, collapse = ".")
+#         if (length(curSeqCh) > 3) {
+#           curLib = curSeqCh[4]
+#           curLib = gsub("[Ll]", "*",  curLib )
+#           curPattern = paste(curMainStr, curLib, sep = ".")
+#         } else {
+#           curPattern = curMainStr
+#         }
+# 
+#         if (!any(grepl(curPattern, curDx))) {
+#           curMissingMetaseq =  c(curMissingMetaseq, curSeq)
+#         }
+#         
+#         if (length(curMissingMetaseq) == 0) {
+#           curMissingMetaseq = NA
+#         }
+#         if (!is.na(curMissingMetaseq)) {
+#           curMissingMetaseq = paste(curMissingMetaseq, collapse = ";")
+#         }
+#         df$Missing_meta_dx[i] = curMissingMetaseq
+#       }
+#     }
+#   }
+#   return(df)
+# }
+
 dxMissing = function(df) {
   df$Missing_meta_dx = NA
   for ( i in 1:nrow(df) ) {
@@ -71,26 +108,91 @@ dxMissing = function(df) {
           curLib = gsub("[Ll]", "*",  curLib )
           curPattern = paste(curMainStr, curLib, sep = ".")
         } else {
-          curPattern = curMainStr
+          curPattern = paste0(curMainStr, "(?![_-].*)")
         }
-
-        if (!any(grepl(curPattern, curDx))) {
+        
+        if (!any(grepl(curPattern, curDx, ignore.case = T, perl = T))) {
           curMissingMetaseq =  c(curMissingMetaseq, curSeq)
         }
-        # stopped here recording if any of the samples are still missing after dx
-        # if length curMissingMetaseq == 0 record as NA otherwise collapse vector into a string
-        if (length(curMissingMetaseq) == 0) {
-          curMissingMetaseq = NA
-        }
-        if (!is.na(curMissingMetaseq)) {
-          curMissingMetaseq = paste(curMissingMetaseq, collapse = ";")
-        }
-        df$Missing_meta_dx[i] = curMissingMetaseq
+        
       }
+      
+      if (length(curMissingMetaseq) == 0) {
+        curMissingMetaseq = NA
+      }
+      if (!is.na(curMissingMetaseq)) {
+        curMissingMetaseq = paste(curMissingMetaseq, collapse = ";")
+      }
+      df$Missing_meta_dx[i] = curMissingMetaseq
     }
   }
   return(df)
 }
+
+
+
+selectPathsMetaseq = function(df) {
+  df$Meta_dx_download = NA
+  for ( i in 1:nrow(df)) {
+    metaseqStr = df$Missing_Metaseq[i]
+    if (!is.na(metaseqStr)) {
+      curMetaseq = strsplit(metaseqStr, ";")[[1]]
+      curDx = strsplit(df$Meta_dx_fastq[i], ";")[[1]]
+      filesComb = character()
+      for (curSeq in curMetaseq) {
+        curSeqCh = strsplit(curSeq, "_")[[1]]
+        curMainStr = curSeqCh[1:3]
+        curMainStr = paste(curMainStr, collapse = ".")
+        if (length(curSeqCh) > 3) {
+          curLib = curSeqCh[4]
+          curLib = gsub("[Ll]", "*",  curLib )
+          curPattern = paste(curMainStr, curLib, sep = ".")
+          curFiles = unique(curDx[grepl(curPattern, curDx, ignore.case = T)])
+        } else if (length(curSeqCh) == 3) {
+          curPattern = paste0(curMainStr, "[_-].*[_-]S")
+          curFiles = unique(curDx[!grepl(curPattern, curDx, ignore.case = T)])
+        }
+        filesComb = c(filesComb, curFiles)
+      }
+      curFilesStr = paste(filesComb, collapse = ";")
+      df$Meta_dx_download[i] = curFilesStr
+    }
+  }
+  return(df)
+}
+
+getFastqPerLib = function(df) {
+  combSeqName = character()
+  combFastq = character()
+  for ( i in 1:nrow(df)) {
+    metaseqStr = df$Missing_Metaseq[i]
+    curMetaseq = strsplit(metaseqStr, ";")[[1]]
+    curDx = strsplit(df$Meta_dx_download[i], ";")[[1]]
+      for (curSeq in curMetaseq) {
+        curFastq = character()
+        curSeqCh = strsplit(curSeq, "_")[[1]]
+        curMainStr = curSeqCh[1:3]
+        curMainStr = paste(curMainStr, collapse = ".")
+        if (length(curSeqCh) > 3) {
+          curLib = curSeqCh[4]
+          curLib = gsub("[Ll]", "*",  curLib )
+          curPattern = paste(curMainStr, curLib, sep = ".")
+          curFiles = unique(curDx[grepl(curPattern, curDx, ignore.case = T)])
+        } else if (length(curSeqCh) == 3) {
+          curPattern = paste0(curMainStr, "[_-].*[_-]S")
+          curFiles = unique(curDx[!grepl(curPattern, curDx, ignore.case = T)])
+        }
+        curFastq = c(curFastq, curFiles)
+        combFilesStr = paste(curFastq, collapse = ";")
+      }
+    combSeqName = c(combSeqName, curSeq)
+    combFastq = c(combFastq, combFilesStr)
+    }
+  combData = data.frame(combSeqName, combFastq)
+  return(combData)
+}
+
+
 
 # current samples table
 curDat = read.csv("SeqSamples_Eval_Depth10.csv")
@@ -110,10 +212,16 @@ rownames(metaDxUpdate) = 1:nrow(metaDxUpdate)
 # update the table if anything is missing after dx search
 metaDxUpdate = dxMissing(df = metaDxUpdate)
 
+# check if any needed samples are not in DNA nexus
 missingDx = metaDxUpdate[!is.na(metaDxUpdate$Missing_meta_dx),]
 
+# extract file names that need to be downloaded
 metaseqDxSamples = metaDxUpdate[!is.na(metaDxUpdate$Missing_Metaseq),]
+metaseqDxSamples = metaseqDxSamples[is.na(metaseqDxSamples$Missing_meta_dx),]
+rownames(metaseqDxSamples) = NULL
+metaseqDxDownload = selectPathsMetaseq(df = metaseqDxSamples)
 
-# if name pattern consists of 3 parts exclude strings with patterns .L..S
-grepl("EHC_C19_1637A([_-].*[_-]S)", "EHC_C19_1637A_S10_R2_001.fastq.gz")
+# represent as one row per Lib with download samples
+metaseqDownloadLibs = getFastqPerLib(df = metaseqDxDownload )
+# check against Annes sheet
 
