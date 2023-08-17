590fb04c4f9cafcde6370cb3b703accc0bc50ef2
abombin <abombin93@gmail.com>
2023-08-17 15:08:56 -0400

    custom motif search

--- Chipseq/Jaspar.R
+++ Chipseq/Jaspar.R
@@ -5,10 +5,12 @@ library(JASPAR2022)
 library(karyoploteR)
 library(monaLisa)
 
+setwd("/home/flyhunter/Kai/Chipseq/pnas/process_custom")
+
 opts <- list()
 
-opts[["species"]] <- 10090
-#opts[["tax_group"]] <- "vertebrates"
+#opts[["species"]] <- 10090
+opts[["tax_group"]] <- "vertebrates"
 
 PFMatrixList <- getMatrixSet(JASPAR2022, opts)
 
@@ -34,10 +36,10 @@ pwms <- toPWM(PFMatrixList)
 
 
 #name(pwms)
-
+# originally used min score 7
 res <- findMotifHits(query = pwms,
                      subject = seqs,
-                     min.score = 7.0,
+                     min.score = "80%",
                      method = "matchPWM",
                      BPPARAM = BiocParallel::SerialParam())
 
@@ -46,4 +48,65 @@ res <- findMotifHits(query = pwms,
 res_df = data.frame(res)
 length(unique(res_df$pwmname))
 
-write.csv(res_df, paste0("motifs/motifs_chr4_", curStart, "_", curEnd, "_jaspar2022_mouse.csv"), row.names = F)
+write.csv(res_df, paste0("motifs/motifs_chr4_", curStart, "_", curEnd, "_jaspar2022_80%_vertebrates.csv"), row.names = F)
+
+
+## custom motifs
+pfms <- getMatrixByID(JASPAR2022, c("MA1621.1", "MA1116.1"))
+pwms <- toPWM(pfms)
+
+res <- findMotifHits(query = pwms,
+                     subject = seqs,
+                     min.score = "80%",
+                     method = "matchPWM",
+                     BPPARAM = BiocParallel::SerialParam())
+
+#unique(res$pwmname)
+res_df = data.frame(res)
+length(unique(res_df$pwmname))
+
+# try with regular biostrins
+startR = c(8647705, 8666955)
+endR = c(8648095, 8667345)
+curStart = startR[2]
+curEnd = endR[2]
+regStart = curStart
+regEnd = curEnd
+curChr = "chr4"
+curRange = paste0(curChr, ":", regStart, "-", regEnd)
+gr <- toGRanges(curRange)
+motifs = c("MA1621.1", "MA1116.1")
+pfms <- getMatrixByID(JASPAR2022, motifs[1])
+pwms <- toPWM(pfms)
+seqs <- getSeq(BSgenome.Mmusculus.UCSC.mm10, gr)
+dna_seq = paste(seqs, collapse = "")
+pwmMat = pwms@profileMatrix
+matches <- Biostrings::matchPWM(pwmMat, dna_seq, min.score="10%")
+matches
+
+### try with homer
+startR = c(8647705, 8666955)
+endR = c(8648095, 8667345)
+curStart = startR[1]
+curEnd = endR[1]
+regStart = curStart
+regEnd = curEnd
+curChr = "chr4"
+curRange = paste0(curChr, ":", regStart, "-", regEnd)
+gr <- toGRanges(curRange)
+seqs <- getSeq(BSgenome.Mmusculus.UCSC.mm10, gr)
+
+pfms <- getMatrixByID(JASPAR2022, c("MA1621.1", "MA1116.1"))
+pwms <- toPWM(pfms)
+
+
+res <- findMotifHits(query = pwms,
+                     subject = seqs,
+                     method = "homer2",
+                     homerfile = "/home/flyhunter/miniconda3/envs/homer/bin/homer",
+                     min.score = 6)
+
+#unique(res$pwmname)
+
+res_df = data.frame(res)
+length(unique(res_df$pwmname))
