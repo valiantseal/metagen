30e60a619e178a478a4672db3650f819478f1407
Charlie Decker <68976294+valiantseal@users.noreply.github.com>
2023-10-23 23:29:33 -0400

    Update confKrakBlast.R

--- metagenClass/v1.3/programs/bin/confKrakBlast.R
+++ metagenClass/v1.3/programs/bin/confKrakBlast.R
@@ -1,57 +1,47 @@
+# Required Libraries
+library(tidyverse)
+library(tidytext)
+
+# Setting the working directory and reading in the data
 dir.create('./output/')
-
-library(dplyr)
-library(stringdist)
-
-# Set the working directory to ./process/R1_001.fastq.gz
 setwd("./process/R1_001.fastq.gz")
 
-# Read the data from blastResTop_1.tsv and krakenSelVirReads.tsv
 blast_df <- read.delim("blastResTop_1.tsv", stringsAsFactors = FALSE, sep = "\t")
 kraken_df <- read.delim("krakenSelVirReads.tsv", stringsAsFactors = FALSE, sep = "\t")
-
-# Merge the data frames based on the "Read" column
-df <- inner_join(blast_df, kraken_df, by = "Read")
-
-# Select and rearrange the desired columns
-df <- df %>%
-  select(Read, stitle, Virus)
-
-df$stitle <- gsub("^\\s+|\\s+$|virus.*$", "virus", trimws(df$stitle), ignore.case = TRUE)
-df$Virus <- gsub("^\\s+|\\s+$|virus.*$", "virus", trimws(df$Virus), ignore.case = TRUE)
-
-# Function to calculate string similarity using Jaccard distance
-calculateSimilarity <- function(stitle, Virus) {
-  similarity <- stringdist::stringdistmatrix(stitle, Virus, method = "jaccard")
-  return(min(similarity) < 0.8)  # Adjust the threshold as needed
-}
-
-# Apply the similarity function to each row
-df$Similarity <- mapply(calculateSimilarity, df$stitle, df$Virus)
-
-# Create a summary table with the actual virus names
-summary_table <- df %>%
-  group_by(Similarity, Virus) %>%
-  summarize(Frequency = n()) %>%
-  mutate(Virus = ifelse(Similarity, Virus, "Mismatch")) %>%
-  select(Virus, Frequency)
-
-# Consolidate all "Mismatch" entries into a single "Mismatch" row
-summary_table <- summary_table %>%
-  group_by(Virus) %>%
-  summarize(Frequency = sum(Frequency)) %>%
-  mutate(Virus = ifelse(Virus != "Mismatch", Virus, "Mismatch")) %>%
-  filter(!(Virus == "Mismatch" & Frequency == 0))
-
-# Rename and create the new "Virus" column
-df <- df %>%
-  rename(Blast = stitle, Kraken = Virus) %>%
-  mutate(Virus = ifelse(Similarity, Kraken, "Mismatch"))
-
 setwd("../../output")
 
-# Save the merged data frame to a new CSV file
-write.csv(summary_table, "krakBlastConfReads_summary.csv", row.names = FALSE)
+df <- inner_join(blast_df, kraken_df, by = "Read")
+df <- df %>%
+  select(Read, stitle, Virus) %>%
+  rename(Blast = stitle, Kraken = Virus)
 
-# Save the modified DataFrame to a new CSV file
-write.csv(df, "krakBlastConfReads.csv", row.names = FALSE)
+# Extract terms from Blast and Kraken columns separately
+blast_terms <- df %>%
+  mutate(Blast_Terms = str_extract_all(Blast, boundary("word"))) %>%
+  unnest(Blast_Terms) %>%
+  select(Blast_Terms)
+
+kraken_terms <- df %>%
+  mutate(Kraken_Terms = str_extract_all(Kraken, boundary("word"))) %>%
+  unnest(Kraken_Terms) %>%
+  select(Kraken_Terms)
+
+# Find common terms and their frequencies
+common_terms <- inner_join(blast_terms, kraken_terms, by = c("Blast_Terms" = "Kraken_Terms")) %>%
+  group_by(Term = Blast_Terms) %>%
+  tally(name = "Frequency") %>%
+  arrange(-Frequency)
+
+# Filter out terms that are shorter than 5 characters and non-viral terms
+non_viral_terms <- c("genome", "cds", "viral", "complete", "segment", "protein", "gene", "isolate", "polymerase", "syndrome", "sequence")
+common_terms <- common_terms %>%
+  filter(nchar(Term) > 5 & !Term %in% non_viral_terms) %>%
+  head(20)
+
+# Generate lookup table
+lookup_table <- common_terms %>%
+  rename(Original_Term = Term) %>%
+  mutate(Standardized_Term = Original_Term)
+
+# Save the lookup table for manual editing
+write.table(lookup_table, "term_mapping.txt", row.names = FALSE, sep = "\t", quote = FALSE)
