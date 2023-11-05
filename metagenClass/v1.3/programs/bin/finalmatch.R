269b09961f7b2111bc2976f87acbc7f69bd59c51
Charlie Decker <68976294+valiantseal@users.noreply.github.com>
2023-11-05 15:39:48 -0500

    Update finalmatch.R

--- metagenClass/v1.3/programs/bin/finalmatch.R
+++ metagenClass/v1.3/programs/bin/finalmatch.R
@@ -10,35 +10,54 @@ library_data <- read.csv("read_label_library.csv")
 krakblast$main_read <- str_extract(krakblast$Read, "^[^/]+")
 library_data$main_read <- str_extract(library_data$ReadID, "^[^/]+")
 
-# Step 1: Compare Paired-End Reads in krakBlastConfReads.csv
+# Compare Paired-End Reads in krakBlastConfReads.csv
 krakblast_grouped <- krakblast %>%
   group_by(main_read) %>%
   summarise(
     virus = toString(unique(Virus)),
-    Blast = first(Blast),  # Assuming that Blast column doesn't require any aggregation
-    Kraken = first(Kraken)  # Assuming that Kraken column doesn't require any aggregation
+    BlastID = first(BlastID),
+    KrakID = first(KrakID)
   ) %>%
-  mutate(result = ifelse(str_count(virus, ",") > 0, "mismatch", virus))
+  mutate(result = ifelse(str_count(virus, ",") > 0, "Mismatch", virus))
 
-# Step 2: Join the new df and read_label_library.csv
+# Join the new df and read_label_library.csv
 final_data <- left_join(library_data, krakblast_grouped, by = "main_read") %>%
-  select(main_read, Label, result, Blast, Kraken) %>%
+  select(main_read, Label, result, BlastID, KrakID) %>%
   mutate(final_result = case_when(
-    is.na(result) ~ "Non-Viral",
+    Label == "Non-Viral" ~ "Non-Viral",
     result == Label ~ as.character(Label),
-    TRUE ~ "mismatch"
+    TRUE ~ "Mismatch"
   ))
 
-# Extract only mismatched data
-mismatch_data <- final_data %>% filter(final_result == "mismatch")
+# Function to calculate metrics
+calculate_metrics <- function(label_col, pred_col) {
+  TP <- sum(final_data[[label_col]] == final_data[[pred_col]], na.rm = TRUE)
+  FP <- sum(final_data[[pred_col]] != "Mismatch" & final_data[[pred_col]] != "Non-Viral" & final_data[[label_col]] != final_data[[pred_col]], na.rm = TRUE)
+  FN <- sum(final_data[[pred_col]] == "Mismatch" | final_data[[pred_col]] == "Non-Viral", na.rm = TRUE)
 
-# Step 3: Create a summary
-summary_data <- final_data %>% 
-  group_by(final_result) %>% 
-  summarise(count = n()) %>% 
-  rename(result = final_result)
+  precision <- TP / (TP + FP)
+  recall <- TP / (TP + FN)
+  f1_score <- 2 * (precision * recall) / (precision + recall)
+  return(c(precision, recall, 1, f1_score))
+}
 
-# Save the final data, mismatch data, and summary data
-write.csv(final_data, "final_data.csv", row.names = FALSE)
-write.csv(mismatch_data, "mismatch_data.csv", row.names = FALSE)
-write.csv(summary_data, "summary_finalmatch.csv", row.names = FALSE)
+# Calculate metrics for Kraken
+kraken_metrics <- calculate_metrics('Label', 'KrakID')
+
+# Calculate metrics for Blast
+blast_metrics <- calculate_metrics('Label', 'BlastID')
+
+# Calculate metrics for the Default method (which we assume is the final_result column)
+default_metrics <- calculate_metrics('Label', 'final_result')
+
+# Create a dataframe for the metrics
+metrics_data <- data.frame(
+  Metric = c('Precision', 'Recall', 'Specificity', 'F1'),
+  Default = default_metrics,
+  Kraken = kraken_metrics,
+  Blast = blast_metrics,
+  stringsAsFactors = FALSE
+)
+
+# Write the data frame to a CSV file
+write.csv(metrics_data, "pipeline_metrics.csv", row.names = FALSE, quote = FALSE)
