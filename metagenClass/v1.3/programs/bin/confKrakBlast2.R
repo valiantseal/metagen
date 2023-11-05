7e7aed8c7e7dfc94430092f37cb2aecb4d398943
Charlie Decker <68976294+valiantseal@users.noreply.github.com>
2023-11-05 14:41:33 -0500

    Update confKrakBlast2.R

--- metagenClass/v1.3/programs/bin/confKrakBlast2.R
+++ metagenClass/v1.3/programs/bin/confKrakBlast2.R
@@ -18,24 +18,33 @@ df <- df %>%
 # Load the mapping table
 term_mapping <- read.table("term_mapping.txt", stringsAsFactors = FALSE, header = TRUE)
 
-# Function to check if both Kraken and BLAST outputs contain either the original or the standardized virus term or a combination of both
-check_virus <- function(blast, kraken, original_term, standardized_term) {
-  if ( (grepl(original_term, blast, ignore.case = TRUE) & grepl(standardized_term, kraken, ignore.case = TRUE)) |
-       (grepl(standardized_term, blast, ignore.case = TRUE) & grepl(original_term, kraken, ignore.case = TRUE)) |
-       (grepl(original_term, blast, ignore.case = TRUE) & grepl(original_term, kraken, ignore.case = TRUE)) |
-       (grepl(standardized_term, blast, ignore.case = TRUE) & grepl(standardized_term, kraken, ignore.case = TRUE))) {
+# Function to check if the Kraken or BLAST output contains the original or the standardized virus term
+find_standardized_term <- function(output, original_term, standardized_term) {
+  if (grepl(original_term, output, ignore.case = TRUE) | grepl(standardized_term, output, ignore.case = TRUE)) {
     return(standardized_term)
   } else {
     return(NA)
   }
 }
 
-# Apply the function for each virus term and each row of the data
+# Initialize KrakID and BlastID with NA
+df$KrakID <- NA
+df$BlastID <- NA
+
+# Apply the function for each virus term and each row of the data to fill KrakID and BlastID
 for (i in 1:nrow(term_mapping)) {
-  df[paste0(term_mapping$Standardized_Term[i], "_match")] <- mapply(
-    check_virus, df$Blast, df$Kraken,
-    MoreArgs = list(original_term = term_mapping$Original_Term[i], standardized_term = term_mapping$Standardized_Term[i])
-  )
+  standardized_term <- term_mapping$Standardized_Term[i]
+  original_term <- term_mapping$Original_Term[i]
+
+  # Update KrakID based on Kraken output
+  krak_matches <- mapply(find_standardized_term, df$Kraken, MoreArgs = list(original_term = original_term, standardized_term = standardized_term))
+  non_na_indices <- which(!is.na(krak_matches))
+  df$KrakID[non_na_indices] <- krak_matches[non_na_indices]
+
+  # Update BlastID based on Blast output
+  blast_matches <- mapply(find_standardized_term, df$Blast, MoreArgs = list(original_term = original_term, standardized_term = standardized_term))
+  non_na_indices <- which(!is.na(blast_matches))
+  df$BlastID[non_na_indices] <- blast_matches[non_na_indices]
 }
 
 # Create a new column 'Virus' initialized to 'Mismatch'
@@ -43,17 +52,15 @@ df$Virus <- "Mismatch"
 
 # Update the 'Virus' column based on matches
 for (i in 1:nrow(df)) {
-  for (j in 1:nrow(term_mapping)) {
-    match_column <- paste0(term_mapping$Standardized_Term[j], "_match")
-    if (!is.na(df[[match_column]][i]) && df[[match_column]][i] != "") {
-      df$Virus[i] <- term_mapping$Standardized_Term[j]
-      break
-    }
+  if (!is.na(df$KrakID[i]) && !is.na(df$BlastID[i]) && df$KrakID[i] == df$BlastID[i]) {
+    df$Virus[i] <- df$KrakID[i]
+  } else {
+    df$Virus[i] <- "Mismatch"
   }
 }
 
 # Write the results to a new CSV
-write.csv(df %>% select(Read, Blast, Kraken, Virus), "krakBlastConfReads.csv", row.names = FALSE)
+write.csv(df %>% select(Read, Blast, Kraken, KrakID, BlastID, Virus), "krakBlastConfReads.csv", row.names = FALSE)
 
 # Count mismatches
 num_mismatches <- nrow(df[df$Virus == "Mismatch", ])
@@ -74,7 +81,6 @@ write.csv(output, "krakBlastConfReads_summary.csv", row.names = FALSE)
 
 # Filter out mismatches
 mismatches_only <- df %>%
-  filter(Virus == "Mismatch")
 
 # Write the mismatches to a CSV
 write.csv(mismatches_only, "krakBlastMismatches.csv", row.names = FALSE)
