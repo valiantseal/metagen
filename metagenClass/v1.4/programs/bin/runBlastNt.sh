8727adf909e82aad44fdffb9465a49dd2f4386cc
Charlie Decker <68976294+valiantseal@users.noreply.github.com>
2023-11-11 03:12:56 -0500

    Delete metagenClass/v1.4/programs directory

--- metagenClass/v1.4/programs/bin/runBlastNt.sh
+++
@@ -1,8 +0,0 @@
-for i in $(cat newdir.list)
-do
-cd process/"$i"/splitSeq10K
-
-ls -d */ | parallel -j 28 'cd {} && sh ../../../../programs/bin/blastNtV4Par.sh'
-
-cd ../../../
-done
--- metagenClass/v1.4/programs/runAnalysis.sh
+++
@@ -1,5 +0,0 @@
-bash -i ./programs/bin/preprep.sh
-
-bash -i ./programs/runAll.sh
-
-Rscript --vanilla ./programs/bin/finalmatch.R
